8d2576b9e6b48392de50d34abe42abbac6d6ad4f
khyw407 <khyw407@gmail.com>
2022-06-11 19:00:41 +0900

    fix: margin 조정

--- src/pages/LandingPages/SignIn/index.js
+++ src/pages/LandingPages/SignIn/index.js
@@ -79,7 +79,7 @@ function SignInBasic() {
       />
       <MKBox px={1} width="100%" height="100vh" mx="auto" position="relative" zIndex={2}>
         <Grid container spacing={1} justifyContent="center" alignItems="center" height="100%">
-          <Grid item xs={11} sm={9} md={5} lg={4} xl={3}>
+          <Grid item xs={11} sm={9} md={5} lg={4} xl={3} mt={3}>
             <Card>
               <MKBox
                 variant="gradient"
